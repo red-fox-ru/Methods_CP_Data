aad021a604125994e82112f3868e198116bfd340
red-F0X <stas-panin@mail.ru>
2021-09-12 15:53:17 +0300

    Correction

---
+++ Parsing_HTML_XPath/parsing_news.py
@@ -0,0 +1,15 @@
+# Les 04
+# Author: Red-F0X (Panin Stanislav)
+# Data create: 11.09.2021
+
+from lxml import html
+import requests
+import unicodedata
+
+
+def get_news():
+    pass
+
+
+if __name__ == '__main__':
+    print(get_news())
